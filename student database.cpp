51a1f506cd5536705b108b964894931a46fb27b3
192111142 <112370088+192111142@users.noreply.github.com>
2022-09-06 10:14:38 +0530

    Add files via upload

---
+++ student database.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class student
+{
+	private
+            	'charname[20]',regd[]
+	             int marks;
+	public
+	             void input();
+	             void display();
+};
+'void student::input()'
+{
+	cout<<"\n name:"<<name;
+	cout<<"\n regd:"<<regd;
+	cout<<"\n branch"<<branch;
+	cout<<"\n marks"<<marks;
+}
+int main()
+{
+	student s;
+	s.input();
+	s.display();
+}
+	             
