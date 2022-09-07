617c0e9d99923cd628a2a7bccef49ce28770b85a
192111142 <112370088+192111142@users.noreply.github.com>
2022-09-07 10:27:15 +0530

    Add files via upload

---
+++ diamond pattern.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   inti,j,odd numbers,space;
+   cout<<"enter the odd numbers:
+   
+   cin>>oddnum;
+   space =odd numbers_;
+   for(i=1;i<=space;j++)
+   {
+      for(j=1;j<=space;j++)
+        cout<<"";
+      space--;
+    for(j=1;j<=(2*i-1);j++)
+       cout<<"*";
+    cout,,endl;
+}
+space=1;
+for(i=1;i<=(oddnum-1);i++)
+{
+   for(j=1;j<=space;j++)
+cout<<" ";
+space++;
+for(j=1;j<=(2*(odd number-i);j++)
+  cout<<endl;
+}
+cout<<endl;
+return 0;
+}
+
+
